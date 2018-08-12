7e6139e9d182d00bae4751515a6d9fdc1731f902
JT Parker <jparkerdev@gmail.com>
2018-08-12 17:10:52 -0600

    complete

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,12 +8,15 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
+// Test that all feeds have associated URLS
 
          it('should have a URL', function() {
            for(let i = 0; i < allFeeds.length; i++) {
              expect(allFeeds[i].url).toBeTruthy();
            }
          });
+
+// Test that all feeds have a name
 
          it('should have a name', function() {
            for(let i = 0; i < allFeeds.length; i++ ) {
@@ -27,9 +30,13 @@ $(function() {
       let bodyElem = $(document.body).get();
       let bodyClass = bodyElem[0].classList;
 
+// Test the menu is hidden by default
+
          it('should be hidden by default', function() {
            expect(bodyClass).toContain('menu-hidden');
          });
+
+// Test the menu toggles open and closed
 
           it('should toggle the menu-hidden class when clicked', function() {
             let menuIcon = $('.menu-icon-link');
@@ -47,6 +54,8 @@ $(function() {
              done();
            });
          });
+
+// Test at least one article is shown in the feed
 
          it('should return at least one .entry', function(done) {
            let feed = $('.feed');
@@ -67,6 +76,8 @@ $(function() {
              done();
            });
          });
+
+// Test the content changes when a new feed is loaded
 
          it('should show new content when a new a new feed is loaded', function() {
            let newFeed = $('.entry');
